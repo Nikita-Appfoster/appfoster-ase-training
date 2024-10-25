a418148549bb3c41e4157735362958bdc79c15c3
Nikita-Appfoster <nikita.kumari@appfoster.com>
2024-10-25 15:56:23 +0530

    misc

---
+++ codingstandard/codingstandard.js
@@ -0,0 +1,30 @@
+// pupose of coding standard 
+
+// consistency
+
+// Readability
+
+// Error Prevvenyion 
+
+// Scalability
+
+// Code Reviews
+// Efficient maintenace 
+//use dry principle 
+// use meaningful name
+// comments
+// avoid using single identifier for multiple purposes
+
+
+
+
+
+
+
+// Steps to test code quality using the BrowserStack Code Quality Management tool:
+
+// Step 1. Sign up for BrowserStack.
+
+// Step 2. Configure project settings.
+
+// Step 3. Upload or connect your codebase.
